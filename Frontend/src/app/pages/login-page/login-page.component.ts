88a3f291647a604c445cbffa6394113cd0a0aab0
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-11-15 10:23:13 +0100

    feat: Add login form without validation

--- Frontend/src/app/pages/login-page/login-page.component.ts
+++ Frontend/src/app/pages/login-page/login-page.component.ts
@@ -9,7 +9,17 @@ export class LoginPageComponent implements OnInit {
 
   constructor() { }
 
+  currentTab = 0;
+
   ngOnInit(): void {
+    this.showTab(this.currentTab); // Display the current tab
+  }
+
+  showTab(n: number) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tab") as HTMLCollectionOf<HTMLElement>;
+    x[n].style.display = "block";
+
   }
 
 }
