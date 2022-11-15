90fc09420dc97382d6bf5f9cdfc77fa74d4230cf
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-11-15 23:25:01 +0100

    quickfix: Add validation to login page form - only frontend

--- Frontend/src/app/pages/login-page/login-page.component.ts
+++ Frontend/src/app/pages/login-page/login-page.component.ts
@@ -22,4 +22,28 @@ export class LoginPageComponent implements OnInit {
 
   }
 
+  validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[this.currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+      // If a field is empty...
+      if (y[i].value == "") {
+        // add an "invalid" class to the field:
+        y[i].className += " invalid";
+        // and set the current valid status to false
+        valid = false;
+      }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+      document.getElementsByClassName("step")[this.currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+  }
+
+
+
 }
