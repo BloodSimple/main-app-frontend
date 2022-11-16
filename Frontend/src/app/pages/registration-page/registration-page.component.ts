0a0616f38fa01a227f29d4cdf2fc59883fa44ff1
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-11-16 21:25:22 +0000

    feat: Add full registration for user with validation

--- Frontend/src/app/pages/registration-page/registration-page.component.ts
+++ Frontend/src/app/pages/registration-page/registration-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registration-page',
@@ -6,13 +9,215 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-page.component.css'],
 })
 export class RegistrationPageComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService
+  ) {}
 
+  registerForm!: FormGroup;
   currentTab = 0;
+  submitted = false;
+
+  //****************************************//
+  // TODO: Extract it - Registration Data:
+  email: any;
+  personalId: any;
+  password: any;
+  confirmPassword: any;
+  firstName: any;
+  lastName: any;
+  gender: any;
+  dateOfBirth: any;
+  job: any;
+  bio: any;
+  isGenderMale: boolean = true;
+  phoneNumber: any;
+  role: any;
+  // address:
+  addressCountry: any;
+  addressCity: any;
+  addressStreet: any;
+  addressNumber: any;
+  //****************************************//
 
   ngOnInit(): void {
     this.showTab(this.currentTab); // Display the current tab
+    this.validateRegistrationForm();
   }
+
+  validateRegistrationForm() {
+    this.registerForm = this.formBuilder.group(
+      {
+        name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.email,
+            Validators.pattern(
+              '[A-Za-z0-9._%-+]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'
+            ),
+          ],
+        ],
+        gender: [],
+        dateOfBirth: [],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(10),
+            Validators.pattern(
+              '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]+)(?=.*[!@#$%^&*()_+~-])[A-Za-zd!@#$%^&*()_+~-].{9,}'
+            ),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required, Validators.minLength(10)]],
+      },
+      {
+        validator: this.checkPasswordWithConfirmedPassword(
+          'password',
+          'confirmPassword'
+        ),
+      }
+    );
+  }
+
+  checkPasswordWithConfirmedPassword(
+    controlName: string,
+    matchingControlName: string
+  ): any {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[controlName];
+      const confirmPasswordControl = formGroup.controls[matchingControlName];
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors['passwordMismatch']
+      ) {
+        return;
+      }
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({
+          checkPasswordWithConfirmedPassword: true,
+        });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    };
+  }
+  checkPasswordRepeatingCharacters() {
+    let passString = this.password.toString();
+    for (let i = 1; i < passString.length - 1; i++) {
+      if (i == passString.length - 2) {
+        if (
+          passString.substring(i - 1, i) == passString.substring(i, i + 1) &&
+          passString.substring(i, i + 1) == passString.substring(i + 1)
+        )
+          return true;
+      } else if (
+        passString.substring(i - 1, i) == passString.substring(i, i + 1) &&
+        passString.substring(i, i + 1) == passString.substring(i + 1, i + 2)
+      )
+        return true;
+    }
+    return false;
+  }
+
+  checkPasswordWithEmail() {
+    if (this.password.toString().includes(this.email) && this.email != '')
+      return true;
+    return false;
+  }
+
+  register() {
+    /*
+    if (this.checkPasswordRepeatingCharacters()) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid password! 🙁',
+        text: 'You have too many repeating characters in a row. Please, pick another password.',
+        background: '#1e2126',
+        color: '#c4c4c4',
+      });
+      return;
+    }
+    if (this.checkPasswordWithEmail()) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid password! 🙁',
+        text: 'Your password contains your e-mail. Please, pick another password.',
+        background: '#1e2126',
+        color: '#c4c4c4',
+      });
+    }
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid input fortmat.',
+        text: 'Some of your inputs had invalid pattern. ',
+        background: '#1e2126',
+        color: '#c4c4c4',
+      });
+      return;
+    } else {
+
+      */
+    this.dateOfBirth = this.dateOfBirth.split('T')[0];
+    var person = {
+      personalId: this.personalId,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      addressCountry: this.addressCountry,
+      addressCity: this.addressCity,
+      addressStreet: this.addressStreet,
+      addressNumber: this.addressNumber,
+      phoneNumber: this.phoneNumber,
+      job: this.job,
+      bio: this.bio,
+      gender: this.isGenderMale ? 'MALE' : 'FEMALE',
+      role: 'USER',
+      dateOfBirth: this.dateOfBirth,
+    };
+    console.log(this.personalId)
+
+
+    console.log("STIGLI SMO DO OVDE 1")
+    this.authenticationService.register(person).subscribe((response) => {
+      console.log("STIGLI SMO DO OVDE 2")
+      if (response.error != '' && response.error != undefined) {
+        console.log(response.error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Something went wrong. 😒',
+          footer: 'Exact error: ' + response.error,
+        });
+      }
+    });
+    Swal.fire({
+      icon: 'success',
+      title: 'Yippee! 🐶',
+      text: this.email + ', you are successfully registrated!',
+      background: '#1e2126',
+      color: '#c4c4c4',
+      showCancelButton: false,
+      showConfirmButton: false,
+      timer: 2000,
+      footer: 'You will be redirected to the homepage...',
+    }).then(function () {
+     // window.location.href = '';
+    });
+    /*}*/
+   // this.close();
+  }
+
+  close(): any {
+    this.close();
+  }
+
+  // TAB Functions:
 
   showTab(n: number) {
     // This function will display the specified tab of the form...
@@ -62,7 +267,8 @@ export class RegistrationPageComponent implements OnInit {
     if (this.currentTab >= x.length) {
       // ... the form gets submitted:
       var regForm = document.getElementById('regForm') as HTMLFormElement;
-      regForm.submit();
+      //regForm.submit();
+      this.register();
       return;
     }
     // Otherwise, display the correct tab:
