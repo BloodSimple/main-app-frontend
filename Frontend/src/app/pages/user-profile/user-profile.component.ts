e28be910cb5414aa877562ac56c2a2b873ee3433
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-11-19 21:36:30 +0100

    quickfix: Change Id to PersonalId

--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,48 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { LoginResponse } from 'src/app/model/LoginResponse';
 import { UserModel } from 'src/app/model/user';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-
   id: string = '';
   email: string = '';
   password: string = '';
   name: string = '';
   surname: string = '';
   gender: string = '';
-  phoneNumber: string='';
-  job: string='';
+  phoneNumber: string = '';
+  job: string = '';
 
   addressStreet: string = '';
   addressNumber: string = '';
   addressCity: string = '';
   addressCountry: string = '';
-  bio: string='';
-//  role: string='';
+  bio: string = '';
+  //  role: string='';
 
-  user= new UserModel();
+  user = new UserModel();
+  activeUser: LoginResponse = new LoginResponse();
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService) {}
 
   ngOnInit(): void {
-
-    this.userService.getUserById("2310987760011").subscribe((response)  => {
+    this.activeUser = this.authenticationService.getCurrentUser();
+    this.userService.getUserById(this.activeUser.personalId).subscribe((response) => {
       console.log(response);
       this.user = response;
-  });
+    });
   }
 
   save(): void {
-
-    this.userService.updateUser(this.user).subscribe((user: UserModel) =>{      
-          this.user = user;    
-          console.log(user);
+    this.userService.updateUser(this.user).subscribe((user: UserModel) => {
+      this.user = user;
+      console.log(user);
     });
   }
 }
