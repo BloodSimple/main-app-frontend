f22bf5410f7fdcc3be0511c70b8d339407fcad9f
mihajlo-ostojic <ostojic.ra134.2018@uns.ac.rs>
2023-06-13 11:30:20 +0200

    fix: misspellings, and missing signs

--- Frontend/src/app/pages/medical-centers/medical-centers.component.ts
+++ Frontend/src/app/pages/medical-centers/medical-centers.component.ts
@@ -22,12 +22,29 @@ export class MedicalCentersComponent implements OnInit {
   constructor(private medicalCenterService: MedicalCenterService) {}
 
   ngOnInit(): void {
-    this.medicalCenterService
-      .getMedicalCenters()
-      .subscribe((medicalCenters: MedicalCenterModel[]) => {
-        this.medicalCenters = medicalCenters;
-        console.log(this.medicalCenters);
-      });
+    // this.medicalCenterService
+    //   .getMedicalCenters()
+    //   .subscribe((medicalCenters: MedicalCenterModel[]) => {
+        
+        
+    //     this.medicalCenters = medicalCenters;
+
+
+    //     console.log(this.medicalCenters);
+    //   });
+      this.medicalCenterService.getMedicalCenters()
+      .subscribe(
+    (medicalCenters: MedicalCenterModel[]) => {
+      this.medicalCenters = medicalCenters;
+      console.log(this.medicalCenters);
+    },
+    (error: any) => {
+      this.inits();
+      // console.error('An error occurred:', error);
+    }
+  );
+
+
   }
 
   onSelect() {
@@ -40,12 +57,17 @@ export class MedicalCentersComponent implements OnInit {
   reset() {
     this.selected = 0;
     this.searchText = '';
-    this.medicalCenterService
-      .getMedicalCenters()
-      .subscribe((medicalCenters: MedicalCenterModel[]) => {
-        this.medicalCenters = medicalCenters;
-        console.log(this.medicalCenters);
-      });
+    this.medicalCenterService.getMedicalCenters()
+      .subscribe(
+    (medicalCenters: MedicalCenterModel[]) => {
+      this.medicalCenters = medicalCenters;
+      console.log(this.medicalCenters);
+    },
+    (error: any) => {
+      this.inits();
+      // console.error('An error occurred:', error);
+    }
+  );
   }
   sortByRatingAsc() {
     this.medicalCenters.sort((a, b) => {
@@ -76,6 +98,57 @@ export class MedicalCentersComponent implements OnInit {
     this.medicalCenters.sort((a, b) => {
       return a.address.city.localeCompare(b.address.city);
     });
+  }
+
+  inits()
+  {
+    
+  // medicalCenter = new MedicalCenterModel();
+    let mcs: MedicalCenterModel[] = [];
+    let medicalCenter1 = new MedicalCenterModel();
+    medicalCenter1.id = '1';
+    medicalCenter1.name ='Blood Drop';
+    let address1 = new AddressModel();
+    address1.id = '2';
+    address1.street = 'Narodnog heroja';
+    address1.number = '32';
+    address1.city = 'Novi Sad';
+    address1.country = 'Srbija';
+    medicalCenter1.address = address1;
+    medicalCenter1.description = 'Vadimo krv kap po kap, kisa sprema se... ok prestacu';
+    medicalCenter1.grade = 0;
+
+    let medicalCenter2 = new MedicalCenterModel();
+    medicalCenter2.id = '1';
+    medicalCenter2.name ='Klinicki Centar Lab - KCL';
+    let address2 = new AddressModel();
+    address2.id = '1';
+    address2.street = 'Scepina ulica';
+    address2.number = 'a2';
+    address2.city = 'Smederevo';
+    address2.country = 'Srbija';
+    medicalCenter2.address = address2;
+    medicalCenter2.description = 'Dosta smo dobar centar za vadjenje krvi, za to smo top';
+    medicalCenter2.grade = 4;
+
+    let medicalCenter3 = new MedicalCenterModel();
+    medicalCenter3.id = '3';
+    medicalCenter3.name ='Medicinski centar Zdravlje';
+    let address3 = new AddressModel();
+    address3.id = '7';
+    address3.street = 'Resavska';
+    address3.number = '3';
+    address3.city = 'Novi Sad';
+    address3.country = 'Srbija';
+    medicalCenter3.address = address3;
+    medicalCenter3.description = 'Najbolji u gradu!';
+    medicalCenter3.grade = 5;
+    
+    this.medicalCenters = [];
+    this.medicalCenters.push(medicalCenter1);
+    this.medicalCenters.push(medicalCenter2);
+    this.medicalCenters.push(medicalCenter3);
+
   }
 
   searchCenters() {
